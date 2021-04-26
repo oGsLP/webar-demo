9b900c69d71bc9451d1aeae775d640132c14ff07
16jsj107 <15275347653@163.com>
2021-04-26 21:50:40 +0800

    #share

--- src/views/share/index.js
+++ src/views/share/index.js
@@ -7,21 +7,25 @@
 
 // stylesheet
 import './index.less';
-
 import html2canvas from 'html2canvas';
-// import './FileSaver';
+import './FileSaver';
 
-// eslint-disable-next-line no-unused-vars
-function Save ()
-{
-    html2canvas(document.body).then(canvas => {
-        // 将canvas内容保存为文件并下载
-        canvas.toBlob(function (blob) {
-            // eslint-disable-next-line no-undef
-            saveAs(blob, 'hangge.png');
-        });
-    });
-}
+alert('!!!');
+// let button = document.getElementsByClassName('div8');
+// button.addEventListener('click', function ()
+// {
+//     alert('hello');
+//     html2canvas(document.body).then(canvas => {
+//         // 将canvas内容保存为文件并下载
+//         console.log('save');
+//         canvas.toBlob(function (blob) {
+//             // eslint-disable-next-line no-undef
+//             saveAs(blob, 'hangge.png');
+//         });
+//     });
+// });
+
+
 
 
 
