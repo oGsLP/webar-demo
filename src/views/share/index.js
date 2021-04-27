5605172ffa846ed92a83520558ea9e3624fafe9b
16jsj107 <15275347653@163.com>
2021-04-27 14:23:50 +0800

    #share

--- src/views/share/index.js
+++ src/views/share/index.js
@@ -10,22 +10,22 @@ import './index.less';
 import html2canvas from 'html2canvas';
 import saveAs from './FileSaver';
 
-// let button = document.getElementsByClassName('div9');
-// // alert(button.length);
-//
-// button.addEventListener('click', function ()
-// {
-//
-//     console.log('hello');
-//     // html2canvas(document.body).then(canvas => {
-//     //     // 将canvas内容保存为文件并下载
-//     //     console.log('save');
-//     //     canvas.toBlob(function (blob) {
-//     //         // eslint-disable-next-line no-undef
-//     //         saveAs(blob, 'hangge.png');
-//     //     });
-//     // });
-// });
+let button = document.getElementsByClassName('div9');
+alert(button.length);
+
+button.addEventListener('click', function ()
+{
+
+    console.log('hello');
+    // html2canvas(document.body).then(canvas => {
+    //     // 将canvas内容保存为文件并下载
+    //     console.log('save');
+    //     canvas.toBlob(function (blob) {
+    //         // eslint-disable-next-line no-undef
+    //         saveAs(blob, 'hangge.png');
+    //     });
+    // });
+});
 
 
 
